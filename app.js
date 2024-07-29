d5869e98456fcc2850a66aecf7c8ea162edb4797
Hiroki Ueno <rockyueno@gmail.com>
2024-07-29 16:43:32 -0700

    submit

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 $(function() {
   // YOUR CODE HERE
+  $('#toggleButton').on('click', () => {
+    $('#myParagraph').toggle();
+  })
+
+  $('#colorButton').on('click', () => {
+    $('#colorDiv').css('background-color', 'yellow');
+  })
+
+  $('#addClassButton').on('click', () => {
+    $('#classDiv').addClass('new');
+  })
+
+  $('#removeClassButton').on('click', () => {
+    $('#classDiv').removeClass('new');
+  })
+
+  $('#fadeInButton').on('click', () => {
+    $('#fadeDiv').fadeIn();
+  })
+
+  $('#fadeOutButton').on('click', () => {
+    $('#fadeDiv').fadeOut();
+  })
+
+  $('#slideUpButton').on('click', () => {
+    $('#slideDiv').slideUp();
+  })
+
+  $('#slideDownButton').on('click', () => {
+    $('#slideDiv').slideDown();
+  })
 })
